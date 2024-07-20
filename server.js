9653950993ef23f42862a122a5948f9e0c11e850
CodeNinja19 <kartiks9999@gmail.com>
2024-07-20 10:53:30 +0530

    changed the vercel.json and cors settings

--- server.js
+++ server.js
@@ -30,7 +30,12 @@ async function main() {
 // testing();
 // app.use section
 
-app.use(cors());
+app.use(cors(
+    {
+        origin:["http://localhost:5173"],
+        methods:["GET","POST"],
+    }
+));
 app.use(express.json({ limit: '50mb' }));
 app.use(cookieParser());
 
