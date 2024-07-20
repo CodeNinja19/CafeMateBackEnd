4abbe8439c4438cddc093508d40a580ff442e867
CodeNinja19 <kartiks9999@gmail.com>
2024-07-20 11:07:15 +0530

    added the default route

--- server.js
+++ server.js
@@ -39,6 +39,9 @@ app.use(cors(
 app.use(express.json({ limit: '50mb' }));
 app.use(cookieParser());
 
+app.get("/", (req, res) => {
+    res.send("Everything is working fine on server for know");
+})
 
 
 app.get("/api", (req, res) => {
